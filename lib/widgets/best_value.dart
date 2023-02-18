550c9af7869e232b968c02e85d72afd69757a3d2
Akash5336 <akashp5226@gmail.com>
2023-02-18 15:16:30 +0530

    Best Crop API Added

--- lib/widgets/best_value.dart
+++ lib/widgets/best_value.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
 
 class BestCrop extends StatefulWidget {
   const BestCrop({super.key});
@@ -10,6 +13,14 @@ class BestCrop extends StatefulWidget {
 
 class _BestCropState extends State<BestCrop> {
   final TextEditingController _cropController = TextEditingController();
+  String N = "Nitrogen: ";
+  String P = "Phosphorus: ";
+  String K = "Pottasium: ";
+  String temp = "Temperature: ";
+  String hum = "Humidity: ";
+  String rain = "Rainfall: ";
+  String pH = "pH: ";
+  String result = "Result";
 
   @override
   void dispose() {
@@ -17,166 +28,214 @@ class _BestCropState extends State<BestCrop> {
     super.dispose();
   }
 
+  bool _isLoading = false;
+
+  Future<String> _getResult(String data) async {
+    final res = await http.post(
+      Uri.parse('http://192.168.137.1:8000/manual.html/bestcrop'),
+      // Uri.parse('http://192.168.1.14:8000/manual.html/bestcrop'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'crop': data,
+      }),
+    );
+    if (res.statusCode == 200) {
+      setState(() {
+        N = "Nitrogen: ${jsonDecode(res.body)["N"]}";
+        P = "Phosporus: ${jsonDecode(res.body)["P"]}";
+        K = "Potassium: ${jsonDecode(res.body)["K"]}";
+        temp = "Temperature: ${jsonDecode(res.body)["Temperature"]}";
+        pH = "pH: ${jsonDecode(res.body)["pH"]}";
+        hum = "Humidity: ${jsonDecode(res.body)["Humidity"]}";
+        rain = "Rainfall: ${jsonDecode(res.body)["Rainfall"]}";
+        _isLoading = false;
+      });
+      return jsonDecode(res.body)["N"].toString();
+    } else {
+      return "Error Fetching Data";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final pageWidth = MediaQuery.of(context).size.width;
     final pageHeight = MediaQuery.of(context).size.height;
+    String data = "";
+    void _getData() {
+      data = _cropController.text.toString().toLowerCase();
+    }
 
     return SafeArea(
-      child: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(
-              top: pageHeight * 0.031,
-              left: pageWidth * 0.178,
-              right: pageWidth * 0.178,
-            ),
-            child: Text(
-              "Best Crop Values",
-              style: GoogleFonts.inter(
-                fontSize: 30,
-                color: const Color(0xff123A32),
-                fontWeight: FontWeight.bold,
+      child: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(
+                color: Color(0xff123A32),
               ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(
-              top: pageHeight * 0.017,
-              left: pageWidth * 0.097,
-              right: pageWidth * 0.097,
-            ),
-            child: Container(
-              height: pageHeight * 0.78,
-              width: pageWidth * 0.81,
-              padding: EdgeInsets.only(
-                  top: pageHeight * 0.04, left: pageWidth * 0.05),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: const Color(0xff65998D),
-                boxShadow: [
-                  BoxShadow(
-                    offset: const Offset(0, 4),
-                    blurRadius: 4,
-                    spreadRadius: 2,
-                    color: Colors.black.withOpacity(0.25),
-                  )
-                ],
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Enter Crop:",
+            )
+          : Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: pageHeight * 0.031,
+                    left: pageWidth * 0.178,
+                    right: pageWidth * 0.178,
+                  ),
+                  child: Text(
+                    "Best Crop Values",
                     style: GoogleFonts.inter(
-                      fontSize: 20,
+                      fontSize: 30,
+                      color: const Color(0xff123A32),
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(top: pageHeight * 0.011),
-                    child: SizedBox(
-                      height: pageHeight * 0.05125,
-                      width: pageWidth * 0.7,
-                      child: TextFormField(
-                        controller: _cropController,
-                        style: GoogleFonts.inder(
-                          fontSize: 20,
-                          color: Colors.white,
-                        ),
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.symmetric(
-                            vertical: pageHeight * 0.005,
-                            horizontal: pageWidth * 0.02,
-                          ),
-                          filled: true,
-                          fillColor: const Color(0xff84AEA4),
-                          enabledBorder: const OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Color(0xff84AEA4),
-                            ),
-                          ),
-                          focusedBorder: const OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Color(0xff84AEA4),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: pageHeight * 0.017,
+                    left: pageWidth * 0.097,
+                    right: pageWidth * 0.097,
                   ),
-                  Padding(
+                  child: Container(
+                    height: pageHeight * 0.78,
+                    width: pageWidth * 0.81,
                     padding: EdgeInsets.only(
-                      top: pageHeight * 0.03625,
-                      left: pageWidth * 0.061,
-                    ),
-                    child: SizedBox(
-                      height: pageHeight * 0.0575,
-                      width: pageWidth * 0.580,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(9),
-                          ),
-                          backgroundColor: const Color(0xff133B33),
-                        ),
-                        onPressed: () {},
-                        child: Text(
-                          "Submit",
-                          style: TextStyle(
-                            fontSize: pageWidth * 0.055,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: pageHeight * 0.08125),
-                    width: pageWidth * 0.705,
-                    height: pageHeight * 0.448,
-                    padding: EdgeInsets.symmetric(
-                      horizontal: pageWidth * 0.075,
-                    ),
+                        top: pageHeight * 0.04, left: pageWidth * 0.05),
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: const Color(0xffB9C6C3).withOpacity(0.55),
+                      borderRadius: BorderRadius.circular(15),
+                      color: const Color(0xff65998D),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: const Offset(0, 4),
+                          blurRadius: 4,
+                          spreadRadius: 2,
+                          color: Colors.black.withOpacity(0.25),
+                        )
+                      ],
                     ),
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Padding(
-                          padding: EdgeInsets.only(
-                            bottom: pageHeight * 0.005,
+                        Text(
+                          "Enter Crop:",
+                          style: GoogleFonts.inter(
+                            fontSize: 20,
                           ),
-                          child: Center(
-                            child: Text(
-                              "Result",
-                              style: GoogleFonts.inter(
-                                fontSize: 28,
-                                color: const Color(0xff123A32),
-                                fontWeight: FontWeight.bold,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: pageHeight * 0.011),
+                          child: SizedBox(
+                            height: pageHeight * 0.05125,
+                            width: pageWidth * 0.7,
+                            child: TextFormField(
+                              controller: _cropController,
+                              style: GoogleFonts.inder(
+                                fontSize: 20,
+                                color: Colors.white,
+                              ),
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.symmetric(
+                                  vertical: pageHeight * 0.005,
+                                  horizontal: pageWidth * 0.02,
+                                ),
+                                filled: true,
+                                fillColor: const Color(0xff84AEA4),
+                                enabledBorder: const OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Color(0xff84AEA4),
+                                  ),
+                                ),
+                                focusedBorder: const OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Color(0xff84AEA4),
+                                  ),
+                                ),
                               ),
                             ),
                           ),
                         ),
-                        const ResultText(text: "Nitrogen:"),
-                        const ResultText(text: "Phosphorous:"),
-                        const ResultText(text: "Potassium:"),
-                        const ResultText(text: "Temperature:"),
-                        const ResultText(text: "Humidity:"),
-                        const ResultText(text: "Rainfall:"),
-                        const ResultText(text: "pH:"),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: pageHeight * 0.03625,
+                            left: pageWidth * 0.061,
+                          ),
+                          child: SizedBox(
+                            height: pageHeight * 0.0575,
+                            width: pageWidth * 0.580,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(9),
+                                ),
+                                backgroundColor: const Color(0xff133B33),
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _isLoading = true;
+                                  _getData();
+                                  _getResult(data);
+                                  result = _cropController.text.toString().toUpperCase();
+                                  _cropController.clear();
+                                });
+                              },
+                              child: Text(
+                                "Submit",
+                                style: TextStyle(
+                                  fontSize: pageWidth * 0.055,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: pageHeight * 0.08125),
+                          width: pageWidth * 0.705,
+                          height: pageHeight * 0.448,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: pageWidth * 0.075,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: const Color(0xffB9C6C3).withOpacity(0.55),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(
+                                  bottom: pageHeight * 0.005,
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    result,
+                                    style: GoogleFonts.inter(
+                                      fontSize: 28,
+                                      color: const Color(0xff123A32),
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              ResultText(text: N),
+                              ResultText(text: P),
+                              ResultText(text: K),
+                              ResultText(text: temp),
+                              ResultText(text: hum),
+                              ResultText(text: rain),
+                              ResultText(text: pH),
+                            ],
+                          ),
+                        )
                       ],
                     ),
-                  )
-                ],
-              ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
     );
   }
 }
